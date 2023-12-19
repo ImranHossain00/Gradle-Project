0cc07edc33feff5707f5cc0a604eb5c88b7edb11
ImranHossain00 <imran188566@gmail.com>
2023-12-20 02:04:58 +0600

    initial commit

--- src/main/java/com/bazlur/eshoppers/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.bazlur.eshoppers.service;
-
-import com.bazlur.eshoppers.modeldto.ProductDTO;
-
-import java.util.List;
-
-public interface ProductService {
-    List<ProductDTO> findAllProductSortByName();
-}
--- src/main/java/com/imran/shopping/cart/service/user/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.imran.shopping.cart.service.user;
-
-import com.imran.shopping.cart.dto.user.UserDTO;
-
-public interface UserService {
-    public void saveUser(UserDTO userDto);
-}
